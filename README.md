e097617e9783af44c8ac8acdeca43fb852c19acb
Imed2023 <IAYEB@scg.com.tn>
2023-07-18 11:16:16 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mayar
+# mayar
---
+++ test.bat
@@ -0,0 +1,7 @@
+echo "# mayar" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Imed2023/mayar.git
+git push -u origin main
